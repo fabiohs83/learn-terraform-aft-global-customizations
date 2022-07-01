78e58c3060e4b40c4044577c2b6e8687832f6b29
code-commit-at-121943979533 <fahbio@gmail.com>
2022-07-01 15:18:49 -0300

    s3 global creation

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,6 +1,17 @@
-resource "aws_s3_account_public_access_block" "block_all_public" {
-  block_public_acls   = true
-  block_public_policy = true
-  ignore_public_acls  = true
-  restrict_public_buckets  = true
+# resource "aws_s3_account_public_access_block" "block_all_public" {
+#   block_public_acls   = true
+#   block_public_policy = true
+#   ignore_public_acls  = true
+#   restrict_public_buckets  = true
+# }
+
+data "aws_caller_identity" "current" {}
+
+resource "aws_s3_bucket" "sandbox_bucket" {
+  bucket = "aft-sandbox-global${data.aws_caller_identity.current.account_id}"
+  acl    = "private"
+  tags = {
+    Name        = "My AFT bucket-${data.aws_caller_identity.current.account_id}"
+    Pipeline    = "aft-global-customizations"
+  }
 }
